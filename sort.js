8628d9109ebd734aa4478ccfb3d622703abf3527
Linley <linleyjfaulkner@gmail.com>
2016-10-06 23:59:44 +0000

    adding assignment

---
+++ sort.js
@@ -0,0 +1,49 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+students.sort(function(a, b) {
+
+var nameA = a.name;
+var nameB = b.name;
+
+	if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  return 0;
+
+});
+
+
+students.sort(function (a, b) {
+
+	var nameA = a.name;
+	var nameB = b.name;
+
+	var ageA = a.age;
+	var ageB = b.age;
+
+	if (nameA === nameB) {
+
+  	if (ageA < ageB) {
+    	return -1;
+  	}
+	  if (ageA < ageB) {
+	    return 1;
+	  }
+
+ 		return 0;
+
+	}
+
+});
+
+console.log(students);
